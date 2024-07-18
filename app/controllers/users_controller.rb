51e078898d1e58664d8daa218c9d319d2bcebea9
EC2 Default User <ec2-user@ip-172-31-10-231.ap-northeast-1.compute.internal>
2024-07-18 01:54:38 +0000

    Chapter7：権限・バリデーションの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,17 @@ class UsersController < ApplicationController
   
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+    if @user.profile_image.attached?
+      blob = @user.profile_image.attachment.blob
+    end
+    if @user.update(user_params)
+      redirect_to user_path(@user.id)
+    else 
+      if @user.profile_image.attached? && @user.profile_image.attachment.blob.present?
+        @user.profile_image.attachment.blob = blob 
+      end
+      render :edit
+    end  
   end
   
   private
